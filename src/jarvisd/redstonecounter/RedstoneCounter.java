0b096d7b55be7836c6dd3ae368bc1ddf31b0479b
Pickle <dill16pickle1993@gmail.com>
2011-07-14 19:45:48 -0400

    Wrote a file sign registery thingy ;3 I yet to test it however.

--- src/jarvisd/redstonecounter/RedstoneCounter.java
+++ src/jarvisd/redstonecounter/RedstoneCounter.java
@@ -7,12 +7,15 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RedstoneCounter extends JavaPlugin	{
+	SignProperties signProp;
     public void onDisable() {
         System.out.println("RedStone Counter Disabled");
     }
     public void onEnable() {
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" ); 
+        
+        signProp = new SignProperties(this, "RC_Signs.conf");
         
         RCBlockRedstoneListener rcListener = new RCBlockRedstoneListener(this);
         PluginManager pm = getServer().getPluginManager();
---
+++ src/jarvisd/redstonecounter/SignProperties.java
@@ -0,0 +1,119 @@
+package jarvisd.redstonecounter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * How this works......
+ * each hashmap contains a hashmap which contains all the data of a single Sign (or anything really)
+ * to get the hash map from the main collection, feed it the key "XYZ" block coordinates.
+ */
+public class SignProperties {
+	private Map<String, Map<String, String>> signsProp = new HashMap<String,Map<String,String>>();
+
+	private File propFile;
+	
+	public SignProperties(JavaPlugin plugin, String file) {
+		propFile = new File(plugin.getDataFolder(), file);
+		if (!plugin.getDataFolder().exists()) { plugin.getDataFolder().mkdir(); }
+		if (!propFile.exists()) { 
+			try {
+				propFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+	
+	public Map<String, String> getMap(String key) {
+		if (signsProp.containsKey(key)) {
+			return signsProp.get(key);
+		}
+		return null;
+	}
+	public void addMap(String key, Map<String, String> map) {
+		if (!signsProp.containsKey(key)) {
+			signsProp.put(key, map);
+		}
+	}
+	
+	public String getString(String key, String keyV) {
+		if (signsProp.containsKey(key)) {
+			Map<String, String> map = signsProp.get(key);
+			if (map.containsKey(keyV)){
+				return map.get(keyV);
+			}
+		}
+		return null;
+	}
+	
+	public void addString(String key, String keyV, String value) {
+		if (signsProp.containsKey(key)) {
+			Map<String, String> map = signsProp.get(key);
+			if (!map.containsKey(keyV)){
+				map.put(keyV, value);
+			}
+		}
+	}
+	
+	/*
+	 * Parses Sign data from a file....
+	 * 
+	 */
+	public void Parse() {
+		try {
+			 BufferedReader inputStream = new BufferedReader(new FileReader(propFile));
+			 String line;
+			while ((line = inputStream.readLine()) != null) {
+				Map<String,String> map = new HashMap<String,String>();
+				String[] Tokens = line.split(" ");
+				String key = null;
+				for (int i = 0; i < Tokens.length; i++) {
+					String[] T = Tokens[i].split("=");
+					map.put(T[0], T[1]);
+					if (T[0].equalsIgnoreCase("X")) { key += T[1]; }
+					else if (T[0].equalsIgnoreCase("Y")) { key += T[1]; }
+					else if (T[0].equalsIgnoreCase("Z")) { key += T[1]; }
+				}
+				signsProp.put(key, map);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void Save() {
+		 try {
+			BufferedWriter writerStream = new BufferedWriter(new FileWriter(propFile));
+			StringBuilder strBuilder = new StringBuilder();
+			for (String key : signsProp.keySet())  {
+				Map<String, String> map = signsProp.get(key);
+				for (String k : map.keySet()) {
+					strBuilder.append(" ");
+					strBuilder.append(k);
+					strBuilder.append("=");
+					strBuilder.append(map.get(k));
+				}
+				strBuilder.append("\n");
+			}
+			writerStream.write(strBuilder.toString());
+		 } catch (FileNotFoundException e) {
+				e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
